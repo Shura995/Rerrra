8d7e36e0f28f535fceb81520dff171446647d54f
Shura995 <pococi995@gmail.com>
2021-08-05 14:52:46 +0200

    matma

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int a = 5;
+	int b = 7;
+	int c = (a+b);
+        System.out.println(c);
+
     }
 }
